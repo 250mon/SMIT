7e938f5a1a78163579696fdb1b7e8588412cb31e
250mon <lambki@naver.com>
2021-10-10 08:41:00 +0900

    evaluation after training is done training class_ious logged checkpoint save_path fixed

--- 2_3_DeepLearning_ObjectDetection/MyICNet/util_ckpt.py
+++ 2_3_DeepLearning_ObjectDetection/MyICNet/util_ckpt.py
@@ -8,16 +8,20 @@ class CheckPtHandler:
         self.network = ic_net.network
         self.session = self.network.session
         self.saver = tf.train.Saver()
+        self.ckpt_dir = self._get_dir()
+
+    def _get_dir(self):
+        os.makedirs(self.settings.ckpt_dir, exist_ok=True)
+        return self.settings.ckpt_dir
 
     def save_ckpt(self, global_step):
-        save_path = os.path.join(self.settings.ckpt_dir)
+        save_path = os.path.join(self.ckpt_dir, 'checkpoint')
         self.saver.save(self.session, save_path, global_step)
         print('\n The checkpoint has been created, epoch: {} \n'.format(global_step))
 
     # restore the most recent checkpoint
     def restore_ckpt(self):
-        ckpt_dir = self.settings.ckpt_dir
-        ckpt = tf.train.get_checkpoint_state(ckpt_dir)
+        ckpt = tf.train.get_checkpoint_state(self.ckpt_dir)
         if ckpt and ckpt.model_checkpoint_path:
             self.saver.restore(self.session, ckpt.model_checkpoint_path)
             ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
@@ -28,8 +32,6 @@ class CheckPtHandler:
             print(" Session starts at epoch - {}".format(start_epoch))
             print("---------------------------------------------------------")
         else:
-            if not os.path.exists(ckpt_dir):
-                os.makedirs(ckpt_dir)
             start_epoch = 0
             print("**********************************************************")
             print("  [*] Failed to find a checkpoint - Start from the first")
